efae47557af80e6afcf32862dbd557473cfcada5
colinxfleming <c3flemin@gmail.com>
2021-09-03 23:07:12 -0400

    man

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,21 +12,21 @@ require 'integration_helper'
 require 'rack/test'
 
 # CI only
-# if ENV['CIRCLECI']
-#   # Use knapsack to split up tests on CI nodes
-#   # To rerack the test divider, run:
-#   # KNAPSACK_GENERATE_REPORT=true bundle exec rake test test:system
-#   require 'knapsack'
-#   knapsack_adapter = Knapsack::Adapters::MinitestAdapter.bind
-#   knapsack_adapter.set_test_helper_path(__FILE__)
+if ENV['CIRCLECI']
+  # Use knapsack to split up tests on CI nodes
+  # To rerack the test divider, run:
+  # KNAPSACK_GENERATE_REPORT=true bundle exec rake test test:system
+  require 'knapsack'
+  knapsack_adapter = Knapsack::Adapters::MinitestAdapter.bind
+  knapsack_adapter.set_test_helper_path(__FILE__)
 
-#   # Activate codecov reporter for test coverage reports
-#   require 'codecov'
-#   SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  # Activate codecov reporter for test coverage reports
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
-#   # Save screenshots if system tests fail
-#   Capybara.save_path = Rails.root.join('tmp', 'capybara')
-# end
+  # Save screenshots if system tests fail
+  Capybara.save_path = Rails.root.join('tmp', 'capybara')
+end
 
 DatabaseCleaner.clean_with :truncation
 
